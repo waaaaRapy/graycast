b41d89ce17ff659daaee047b66f81236713b9e95
waaaaRapy <kk3ws.as@gmail.com>
2020-07-11 16:11:24 +0900

    swap test case arguments
    assert $expected $input -> assert $input $expected

--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 assert() {
-  expected="$1"
-  input="$2"
+  input="$1"
+  expected="$2"
 
   ./graycast "$input" > tmp.s
   cc -o tmp tmp.s
@@ -17,14 +17,14 @@ assert() {
   fi
 }
 
-assert 0 0
-assert 57 57
-assert 21 "5+20-4"
-assert 21 "5 + 20 - 4"
-assert 47 "5+6*7"
-assert 15 "5 * (9-6)"
-assert 4  "(3+5)/2"
-assert 0  "-1+(+1)"
-assert 15 "-3*-5"
-assert 20  "10--10"
+assert "0" 0
+assert "57" 57
+assert "5+20-4" 21
+assert "5 + 20 - 4" 21
+assert "5+6*7" 47
+assert "5 * (9-6)" 15
+assert "(3+5)/2" 4
+assert "-1+(+1)" 0
+assert "-3*-5" 15
+assert "10--10" 20
 echo OK
